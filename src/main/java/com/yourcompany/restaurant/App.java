99f60e1b7fd421fd77f37c3144739ab49df509e6
allyelvis <allyelvis6569@gmail.com>
2024-10-27 19:31:47 +0000

    initial

---
+++ src/main/java/com/yourcompany/restaurant/App.java
@@ -0,0 +1,13 @@
+package com.yourcompany.restaurant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
